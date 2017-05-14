406829f6aeb27f3789ed015c855346125294aac0
Joachim Nilsson <troglobit@gmail.com>
2017-05-15 01:03:02 +0200

    Use actual uuid:<UUID> in USN instead of hard coded hex value
    Signed-off-by: Joachim Nilsson <troglobit@gmail.com>

--- ssdpd.c
+++ ssdpd.c
@@ -694,6 +694,7 @@ int main(int argc, char *argv[])
 	uuidgen();
 	lsb_init();
 
+	strncpy(ctx.header.unique_service_name, uuid, sizeof(ctx.header.unique_service_name));
 	strncpy(ctx.header.server_string, server_string, sizeof(ctx.header.server_string));
 //	for (i = optind; i < argc; i++)
 //		open_ssdp_socket(argv[i]);
