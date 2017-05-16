86790dac1e601c1925c02be71bc46432309fc18a
Joachim Nilsson <troglobit@gmail.com>
2017-05-16 21:01:47 +0200

    Handle IGMP join fails on interface with multiple addresses
    In setups with multiple IP addresses on the same interface, ssdpd will
    fail to join the same multicast group.  This is OK, so do not warn the
    user unnecessarily.
    
    Signed-off-by: Joachim Nilsson <troglobit@gmail.com>

--- ssdpd.c
+++ ssdpd.c
@@ -508,6 +508,9 @@ static int multicast_join(int sd, struct sockaddr *sa)
 	mreq.imr_address = sin->sin_addr;
 	mreq.imr_multiaddr.s_addr = inet_addr(MC_SSDP_GROUP);
         if (setsockopt(sd, IPPROTO_IP, IP_ADD_MEMBERSHIP, &mreq, sizeof(mreq))) {
+		if (EADDRINUSE == errno)
+			return 0;
+
 		logit(LOG_ERR, "Failed joining group %s: %s", MC_SSDP_GROUP, strerror(errno));
 		return -1;
 	}
