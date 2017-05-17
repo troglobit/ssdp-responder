03485c572253545f5109e2c25c1732804271c951
Joachim Nilsson <troglobit@gmail.com>
2017-05-17 14:01:47 +0200

    Fix regression in v1.4, allow running without interface filtering
    The new interface filtering feature of v1.4 managed to filter out *all*
    interfaces if none were given on the command line.
    
    Signed-off-by: Joachim Nilsson <troglobit@gmail.com>

--- ssdpd.c
+++ ssdpd.c
@@ -251,6 +251,11 @@ static int filter_iface(char *ifname, char *iflist[], size_t num)
 {
 	size_t i;
 
+	if (!num) {
+		logit(LOG_DEBUG, "No interfaces to filter, using all with an IP address.");
+		return 0;
+	}
+
 	logit(LOG_DEBUG, "Filter %s?  Comparing %zd entries ...", ifname, num);
 	for (i = 0; i < num; i++) {
 		logit(LOG_DEBUG, "Filter %s?  Comparing with %s ...", ifname, iflist[i]);
