8c22bef6a8cdf04aa4338825e0a74063a8f37e7b
Joachim Nilsson <troglobit@gmail.com>
2018-06-08 18:00:36 +0200

    Interface may not have an IP address (yet)
    Signed-off-by: Joachim Nilsson <troglobit@gmail.com>

--- ssdpd.c
+++ ssdpd.c
@@ -108,6 +108,9 @@ static struct ifsock *find_iface(struct sockaddr *sa)
 	struct ifsock *ifs;
 	struct sockaddr_in *addr = (struct sockaddr_in *)sa;
 
+	if (!sa)
+		return NULL;
+
 	LIST_FOREACH(ifs, &il, link) {
 		if (ifs->addr.sin_addr.s_addr == addr->sin_addr.s_addr)
 			return ifs;
