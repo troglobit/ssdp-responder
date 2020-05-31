01e820c83790c150c9261e7f03674236c7328516
Joachim Nilsson <troglobit@gmail.com>
2020-05-31 22:33:50 +0200

    check.sh: Support BusyBox ifconfig and systems with only iproute2
    Signed-off-by: Joachim Nilsson <troglobit@gmail.com>

--- check.sh
+++ check.sh
@@ -8,7 +8,15 @@ if [ "x$V" = "x1" ]; then
 fi
 
 # Loopback on Linux: lo, on FreeBSD: lo0
-LOOPBACK=`ifconfig |grep LOOPBACK |sed 's/\([lo0-9]*\):.*/\1/'`
+which ip >/dev/null
+if [ $? -ne 0 ]; then
+    # This works with standard ifconfig on Linux and *BSD and
+    # non-standard ifconfig from BusyBox
+    LOOPBACK=`ifconfig |grep -i LOOPBACK |head -1 | sed 's/\([lo0-9]*\).*/\1/'`
+else
+    # New Debian/Ubuntu systems don't have ifconfig
+    LOOPBACK=`ip link | grep LOOPBACK | sed 's/[0-9]*: \([lo0-9]*\):.*/\1/'`
+fi
 
 ./src/ssdpd -n $LOOPBACK &
 PID=$!
