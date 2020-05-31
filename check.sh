8b6e006009a03ee6236bcfc4f9da7b5492404137
Joachim Nilsson <troglobit@gmail.com>
2020-05-31 22:53:10 +0200

    check.sh: Check for ifconfig before set -e ...
    Signed-off-by: Joachim Nilsson <troglobit@gmail.com>

--- check.sh
+++ check.sh
@@ -1,18 +1,18 @@
 #!/bin/sh
-set -e
+IFCONF=`which ifconfig`
 Q=-q
 
+set -e
 if [ "x$V" = "x1" ]; then
     set -x
     Q=""
 fi
 
 # Loopback on Linux: lo, on FreeBSD: lo0
-which ip >/dev/null
-if [ $? -ne 0 ]; then
+if [ -n "$IFCONF" ]; then
     # This works with standard ifconfig on Linux and *BSD and
     # non-standard ifconfig from BusyBox
-    LOOPBACK=`ifconfig |grep -i LOOPBACK |head -1 | sed 's/\([lo0-9]*\).*/\1/'`
+    LOOPBACK=`$IFCONF |grep -i LOOPBACK |head -1 | sed 's/\([lo0-9]*\).*/\1/'`
 else
     # New Debian/Ubuntu systems don't have ifconfig
     LOOPBACK=`ip link | grep LOOPBACK | sed 's/[0-9]*: \([lo0-9]*\):.*/\1/'`
